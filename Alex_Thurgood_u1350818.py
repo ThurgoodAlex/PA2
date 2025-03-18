3ef0327b8e201649c8f09027e2bb2ecd05fe33a2
ThurgoodAlex <adthurgood@gmail.com>
2025-03-18 17:42:35 -0600

    added documentation

--- Alex_Thurgood_u1350818.py
+++ Alex_Thurgood_u1350818.py
@@ -5,6 +5,7 @@ log = core.getLogger()
 
 class LoadBalancer(object):
     def __init__(self):
+        """This creates and sets up the ip addresses and ports for h1 and h5"""
         core.openflow.addListeners(self)
         self.h1_ip = IPAddr("10.0.0.10")
         self.h5_ip = IPAddr("10.0.0.5")
@@ -18,12 +19,12 @@ class LoadBalancer(object):
     
     def setup_rules(self):
         """This sets up the openflow to allow ARP packets and creates the rules from h1->h5 and vice versa"""
-        
+
         arp_rule = of.ofp_flow_mod()
         arp_rule.match.dl_type = 0x0806
         arp_rule.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
         self.connection.send(arp_rule)
-        log.info("Installed ARP handling rule")
+        log.info("Installed ARP rule")
 
 
         h1_to_h5 = of.ofp_flow_mod()
@@ -32,7 +33,7 @@ class LoadBalancer(object):
         h1_to_h5.match.nw_dst = self.h5_ip
         h1_to_h5.actions.append(of.ofp_action_output(port=self.h5_port))
         self.connection.send(h1_to_h5)
-        log.info("Installed flow rule: h1 -> h5")
+        log.info("Installed flow rule from h1 -> h5")
         
         h5_to_h1 = of.ofp_flow_mod()
         h5_to_h1.match.in_port = self.h5_port
@@ -40,8 +41,7 @@ class LoadBalancer(object):
         h5_to_h1.match.nw_dst = self.h1_ip 
         h5_to_h1.actions.append(of.ofp_action_output(port=self.h1_port))
         self.connection.send(h5_to_h1)
-        log.info("Installed flow rule: h5 -> h1")
+        log.info("Installed flow rule from h5 -> h1")
         
 def launch():
     core.registerNew(LoadBalancer)
-    log.info("LoadBalancer application started!")
